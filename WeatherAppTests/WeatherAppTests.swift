4c37383dc425198e1333568624010451a0314a9a
yesidsossa <steven.uni21@gmail.com>
2025-01-30 17:29:18 -0500

    chore(structure): setup base architecture folder structure

---
+++ WeatherAppTests/WeatherAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppTests.swift
+//  WeatherAppTests
+//
+//  Created by Yesid Hernandez on 30/01/25.
+//
+
+import Testing
+@testable import WeatherApp
+
+struct WeatherAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
